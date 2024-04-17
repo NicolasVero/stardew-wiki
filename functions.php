55ea4899375795920061665936293691c7cfc901
Nicolas VERO <123495341+NicolasVero@users.noreply.github.com>
2024-04-17 16:12:56 +0200

    ajout d'elements statistiques aux joueurs

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function log_($element) {
 
 
 
-function get_all_players($data): array {
+function get_all_players(object $data): array {
     $players = array();
 
     array_push($players, get_aggregated_data($data->player));
@@ -19,25 +19,51 @@ function get_all_players($data): array {
 }
 
 
-function get_aggregated_data($data):array {
+function get_aggregated_data(object $data):array {
     
     // log_($data);
 
     return array(
-        'favorite_thing' => $data->favoriteThing,
-        'farming_level' => $data->farmingLevel
+        'favorite_thing' => (string) $data->favoriteThing,
+        'farming_level'  => (int) $data->farmingLevel,
+        'mining_level'   => (int) $data->miningLevel,
+        'combat_level'   => (int) $data->combatLevel,
+        'foraging_level' => (int) $data->foragingLevel,
+        'fishing_level'  => (int) $data->fishingLevel,
+        'luck_level'     => (int) $data->luckLevel,
+        'max_items'      => (int) $data->maxItems,
+        'max_health'     => (int) $data->maxHealth,
+        'max_stamina'    => (int) $data->maxStamina,
+        'gender'         => (string) $data->gender,
+        'friendship'     => get_friendship_data($data->friendshipData),
+        'monsters_kill'  => get_monsters_kill_data($data->stats)
     );
+}
 
-    // favoriteThing
-    // farmingLevel	:	1
-    // miningLevel	:	3
-    // combatLevel	:	2
-    // foragingLevel	:	2
-    // fishingLevel	:	4
-    // luckLevel	:	0
-    // maxStamina	:	270
-    // maxItems	:	24
-    // maxHealth	:	110
-    // gender	:	Male
-    // friendshipData
+
+function get_monsters_kill_data(object $data): array { 
+    $monsters = [];
+    
+    foreach ($data->specificMonstersKilled->item as $item) {
+        $monsters[(string) $item->key->string] = (int) $item->value->int;
+    }
+    
+    return $monsters;
+}
+
+
+function get_friendship_data(object $data):array { 
+    
+    $friends = array();
+
+    foreach($data->item as $item) {
+        $friends[(string) $item->key->string] = array(
+
+            'points'     => (int) $item->value->Friendship->Points,
+            'status'     => (int) $item->value->Friendship->Status,
+            'week_gifts' => (int) $item->value->Friendship->GiftsThisWeek
+        );
+    }
+
+    return $friends; 
 }
