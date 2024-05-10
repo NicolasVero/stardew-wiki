e9a03fba608c898725bb223fc8150a45cb970ebd
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2024-05-11 00:27:51 +0200

    suppression des sauvegardes locales

--- includes/get_xml_data.php
+++ includes/get_xml_data.php
@@ -16,7 +16,9 @@ if($_FILES['save-upload']['error'] === UPLOAD_ERR_OK && is_file_secure($_FILES['
 
     $players_data = get_all_players_datas($data);
     $players = get_all_players($data);
-   
+    
+    
+
     $response['html'] = display_page($players_data[0], $players);
     $response['code'] = "success";
 } else {
