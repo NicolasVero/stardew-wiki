976239442d61db48085dcfb8176e723022e44928
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2024-05-11 23:03:59 +0200

    ajout boites dialogues erreur et ajout throw Exception

--- includes/get_xml_data.php
+++ includes/get_xml_data.php
@@ -17,20 +17,50 @@ require 'security_check.php';
 //&NEW 
 $response = array();
 
-if($_FILES['save-upload']['error'] === UPLOAD_ERR_OK && is_file_secure($_FILES['save-upload'])) {
+try {
+    if(is_file_secure($_FILES['save-upload'])) {
+        $uploadedFile = $_FILES['save-upload']['tmp_name'];
+        $data = simplexml_load_file($uploadedFile);
 
-    $uploadedFile = $_FILES['save-upload']['tmp_name'];
-    $data = simplexml_load_file($uploadedFile);
+        $players_data = get_all_players_datas($data);
+        $players = get_all_players($data);
+        
 
-    $players_data = get_all_players_datas($data);
-    $players = get_all_players($data);
-    
-
-    $response['html'] = display_page($players_data[0], $players);
-    $response['code'] = "success";
-} else {
+        $response['html'] = display_page($players_data[0], $players);
+        $response['code'] = "success";
+    }
+} catch (Exception $exception) {
     $response['html'] = "Something went wrong";
     $response['code'] = "failed";
 }
 
 echo json_encode($response);
+
+
+
+
+
+
+
+
+
+
+
+
+// if(is_file_secure($_FILES['save-upload'])) {
+
+//     $uploadedFile = $_FILES['save-upload']['tmp_name'];
+//     $data = simplexml_load_file($uploadedFile);
+
+//     $players_data = get_all_players_datas($data);
+//     $players = get_all_players($data);
+    
+
+//     $response['html'] = display_page($players_data[0], $players);
+//     $response['code'] = "success";
+// } else {
+//     $response['html'] = "Something went wrong";
+//     $response['code'] = "failed";
+// }
+
+// echo json_encode($response);
--- includes/security_check.php
+++ includes/security_check.php
@@ -1,15 +1,27 @@
 <?php 
 
-function is_file_secure(mixed $file):bool {
+function is_file_secure(mixed $file): bool {
+
+    if(!is_uploaded_file($file['tmp_name'])) {
+        throw new Exception('Erreur lors du chargement du fichier.');
+    }
+
+    if($file['error'] !== UPLOAD_ERR_OK) {
+        throw new Exception('Erreur lors du téléchargement du fichier.');
+    }
+
+    if(!($file['size'] > 0 && $file['size'] < in_bytes_conversion("40Mo"))) {
+        throw new Exception('Taille de fichier invalide.');
+    }
+
     $file_content = (array) simplexml_load_file($file['tmp_name']);
-    
-    $check_file_error  = $file['error'] == 0;
-    $check_file_size   = $file['size'] > 0 && $file['size'] < in_bytes_conversion("40Mo"); 
-    $is_a_stardew_save = 
-        array_key_exists('player', $file_content) && 
+    if(!(
+        array_key_exists('player', $file_content) &&
         array_key_exists('uniqueIDForThisGame', $file_content) &&
         array_key_exists('gameVersion', $file_content)
-    ; 
+    )) {
+        throw new Exception('Fichier non conforme à une sauvegarde Stardew Valley.');
+    }
 
-    return $check_file_error && $check_file_size && $is_a_stardew_save;
+    return true;
 }
