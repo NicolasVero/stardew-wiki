63abe291a2eb37df94b77a05e2f6f54657132807
Romain <gerarromain@gmail.com>
2024-05-13 10:20:14 +0200

    fix security details

--- includes/security_check.php
+++ includes/security_check.php
@@ -2,6 +2,14 @@
 
 function is_file_secure(mixed $file): bool {
 
+    if(!array_keys_exists(array('player', 'uniqueIDForThisGame', 'gameVersion'), (array) simplexml_load_file($file['tmp_name']))) {
+        throw new Exception('File not conforming to a Stardew Valley save.');
+    }
+
+    if(!($file['size'] > 0 || $file['size'] < in_bytes_conversion("40Mo"))) {
+        throw new Exception('Invalid file size.');
+    }
+
     if(!is_uploaded_file($file['tmp_name'])) {
         throw new Exception('Error loading file.');
     }
@@ -10,13 +18,6 @@ function is_file_secure(mixed $file): bool {
         throw new Exception('Error downloading file.');
     }
 
-    if(!($file['size'] > 0 && $file['size'] < in_bytes_conversion("40Mo"))) {
-        throw new Exception('Invalid file size.');
-    }
-
-    if(!array_keys_exists(array('player', 'uniqueIDForThisGame', 'gameVersion'), (array) simplexml_load_file($file['tmp_name']))) {
-        throw new Exception('File not conforming to a Stardew Valley save.');
-    }
 
     return true;
 }
