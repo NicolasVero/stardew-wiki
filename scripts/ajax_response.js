c02f44e27b6e2e26485b5f977f32cb0614e226ea
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2024-11-22 21:00:53 +0100

    changement synthaxe js
    Co-authored-by: BreadyBred <gerarromain@gmail.com>

--- scripts/ajax_response.js
+++ scripts/ajax_response.js
@@ -11,21 +11,22 @@ async function AJAX_send() {
 	const landing_menu = document.getElementById("landing_menu");
 	const landing_page = document.getElementById("landing_page").outerHTML;
 
-	if(landing_menu)
+	if(landing_menu) {
 		landing_menu.outerHTML = "";
+	}
 
 	page_display.innerHTML = "";
 
 	if(file) {
-
 		let form_data = new FormData();
 		let xhr       = new XMLHttpRequest();
 		const url     = get_site_root() + "includes/get_xml_data.php";
 		
-		if(is_file_too_big)
+		if(is_file_too_big) {
 			form_data.append("save-upload", new File(["SizeException"], "Error_SizeException.xml"));
-		else
+		} else {
 			form_data.append("save-upload", file);
+		}
 
 		xhr.open("POST", url, true);
 
@@ -42,8 +43,9 @@ async function AJAX_send() {
 					page_display.innerHTML = html["sur_header"];
 					page_display.innerHTML += landing_page;
 
-					for(let i = 0; i < players_count; i++)
+					for(let i = 0; i < players_count; i++) {
 						page_display.innerHTML += html["player_" + i];
+					}
 
 					initialize_player_swapper(players_count);
 
@@ -79,10 +81,11 @@ function activate_feedback_ajax_trigger() {
 			hide_all_sections();
 
 			const existing_window = document.querySelector(".feedback-panel");
-			if(existing_window)
+			if(existing_window) {
 				toggle_visibility(existing_window, true);
-			else
+			} else {
 				feedback_form_creation();
+			}
 		});
 	})
 }
@@ -107,8 +110,9 @@ function feedback_form_creation() {
 		const tempContainer = document.createElement("div");
 		tempContainer.innerHTML = data;
 
-		while (tempContainer.firstChild)
+		while(tempContainer.firstChild) {
 			xml_upload.appendChild(tempContainer.firstChild);
+		}
 
 		current_section = document.querySelector(".feedback-panel");
 
@@ -132,12 +136,8 @@ function activate_feedback_form() {
 		})
 		.then(response => response.json())
 		.then(data => {
-			console.log(data);
-			if(data.success) {
-				alert(data.message);
-			} else {
-				alert("Error submitting form : " + data.message);
-			}
+			const alert_message = (data.success) ? data.message : "Error submitting form : " + data.message;
+			alert(alert_message);
 		})
 		.catch(error => {
 			console.error("Error : ", error);
