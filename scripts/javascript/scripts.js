bbc7ba1e49f22e78b5f380f24f4f2bc004e9dd2b
Nicolas Vero <nicolasvero03@gmail.com>
2024-12-02 19:20:37 +0100

    ajout option steam achievements

--- scripts/javascript/scripts.js
+++ scripts/javascript/scripts.js
@@ -89,6 +89,7 @@ window.addEventListener("load", () => {
     const toggle_versions_items_mode = document.getElementById("toggle_versions_items_mode");
     const no_spoil_mode = document.getElementById("no_spoil_mode");
     const spoil_mode = document.getElementById("spoil_mode");
+    const steam_achievements = document.getElementById("steam_achievements");
     if (toggle_versions_items_mode) {
         toggle_versions_items_mode.addEventListener("change", handle_toggle_versions_mode);
     }
@@ -97,6 +98,9 @@ window.addEventListener("load", () => {
     }
     if (spoil_mode) {
         spoil_mode.addEventListener("change", handle_spoil_mode);
+    }
+    if (steam_achievements) {
+        steam_achievements.addEventListener("change", handle_steam_mode);
     }
     const save_upload = document.getElementById("save-upload");
     if (save_upload) {
@@ -135,6 +139,15 @@ function handle_spoil_mode() {
     }
 }
 ;
+function handle_steam_mode() {
+    const images_folder = ["steam_achievements", "achievements"];
+    const images = document.querySelectorAll(".achievements-section img");
+    images.forEach((image) => {
+        const src = image.getAttribute("src");
+        const [old_folder, new_folder] = (src.includes('steam')) ? images_folder : [...images_folder].reverse();
+        image.setAttribute("src", src.replace(old_folder, new_folder));
+    });
+}
 function initialize_settings() {
     handle_toggle_versions_mode();
     handle_no_spoil_mode();
