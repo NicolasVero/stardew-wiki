c7e7eea506ecd3ab1dabefb353adc2ba80e7aa6b
Nicolas Vero <nicolasvero03@gmail.com>
2024-12-05 00:55:50 +0100

    changement indentation

--- includes/page_loader.php
+++ includes/page_loader.php
@@ -3,7 +3,6 @@
 if(is_on_localhost() && isset($_GET["dev"])) {
     $file = (does_save_exists($_GET["dev"])) ? $_GET["dev"] : "default";
     load_save(get_saves_folder() . "/$file", false);
-}
-else {
+} else {
     echo display_landing_page();
 }
