39fe0ac84ab4b08a0c722a4a005da475fc54a5ed
Romain <gerarromain@gmail.com>
2024-12-06 13:43:09 +0100

    Update quantity dans les bundles

--- includes/functions/display_other_functions.php
+++ includes/functions/display_other_functions.php
@@ -347,7 +347,7 @@ function display_bundle_requirements(array $requirements, array $added_items):st
 
         $formatted_item_name = formate_text_for_file($name);
         $has_been_donated = (has_been_donated_in_bundle($name, $added_items)) ? "donated" : "not-donated";
-        $quantity = ($quantity > 1 && $quantity < 10) ? "<span class='quantity'>$quantity</span>" : "";
+        $quantity = ($quantity > 1) ? "<span class='quantity'>$quantity</span>" : "";
 
         $structure .= "
             <span class='required-item'>
