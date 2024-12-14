8154d13f9c24059a0621b9b3f8867fe206c713c8
Romain <gerarromain@gmail.com>
2024-12-14 14:58:27 +0100

    Consistence typescript part 2
    Co-authored-by: NicolasVero <nicolasvero03@gmail.com>

--- scripts/typescript/app.ts
+++ scripts/typescript/app.ts
@@ -38,11 +38,9 @@ window.addEventListener("load", () => {
     }
 
     save_landing_surheader();
-    
     activate_buttons(".landing-upload", ".exit-upload", ".upload-panel");
     activate_buttons(".landing-settings", ".exit-settings", ".settings");
     toggle_custom_checkboxes(".checkmark");
-
     activate_feedback_ajax_trigger();
 });
 
